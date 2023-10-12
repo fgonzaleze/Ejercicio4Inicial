25292fc61b91920d87858488ac576af84ae3fc8e
34611 <34611@AJARE>
2023-10-12 18:47:23 +0200

    Agregar archivos de proyecto.

---
+++ Ejercicio4Inicial/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace Ejercicio4Inicial
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void elegirFecha(object sender, DateChangedEventArgs e)
+        {
+            // Cuando se selecciona una fecha de entrada, habilitar el DatePicker de salida
+            fechaSalida.MinimumDate = fechaEntrada.Date;
+            fechaSalida.IsEnabled = true;
+        }
+    }
+}
